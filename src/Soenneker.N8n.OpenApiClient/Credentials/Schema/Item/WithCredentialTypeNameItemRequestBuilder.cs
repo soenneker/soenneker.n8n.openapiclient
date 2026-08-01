fa2789be5ebc5c353f37dca9a2c7fa28fc8ec9aa
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-01 17:57:39 +0000

    Automated update

--- src/Soenneker.N8n.OpenApiClient/Credentials/Schema/Item/WithCredentialTypeNameItemRequestBuilder.cs
+++ src/Soenneker.N8n.OpenApiClient/Credentials/Schema/Item/WithCredentialTypeNameItemRequestBuilder.cs
@@ -36,20 +36,20 @@ namespace Soenneker.N8n.OpenApiClient.Credentials.Schema.Item
         /// <summary>
         /// Show credential data schema
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public async Task<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson?> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema?> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #nullable restore
 #else
-        public async Task<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema> GetAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #endif
             var requestInfo = ToGetRequestInformation(requestConfiguration);
-            return await RequestAdapter.SendAsync<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson>(requestInfo, global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema>(requestInfo, global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Show credential data schema
--- src/Soenneker.N8n.OpenApiClient/Models/GetCredentialsSchemaByCredentialTypeName200ResponseSchema.cs
+++ src/Soenneker.N8n.OpenApiClient/Models/GetCredentialsSchemaByCredentialTypeName200ResponseSchema.cs
@@ -9,27 +9,27 @@ namespace Soenneker.N8n.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson : IAdditionalDataHolder, IParsable
+    public partial class GetCredentialsSchemaByCredentialTypeName200ResponseSchema : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema"/> and sets the default values.
         /// </summary>
-        public GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson()
+        public GetCredentialsSchemaByCredentialTypeName200ResponseSchema()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseResponseJson();
+            return new global::Soenneker.N8n.OpenApiClient.Models.GetCredentialsSchemaByCredentialTypeName200ResponseSchema();
         }
         /// <summary>
         /// The deserialization information for the current model
